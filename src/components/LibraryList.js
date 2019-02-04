6f7ee6b8409624d17a337a982a5e83bea4c69509
jamalfazlur <fazlur.jamal@gmail.com>
2019-02-04 12:05:49 +0700

    Finish

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
     
-    renderNama = () => {
-        var listNama = this.props.kataKu.map((item) => {
-            return <Text>{item}</Text>
-        })
-        return listNama;
+    // componentWillMount() {
+    //     const ds = new ListView.DataSource({
+    //         rowHasChanged : (r1, r2) => r1 !== r2
+    //     });
+    //     this.dataSource = ds.cloneWithRows(this.props.libraries);
+    // }
+
+    renderRow = ({item}) => {
+        //return <Text>{item.title}</Text>
+        return <ListItem library={item} />
+
     }
 
     render() {
         return(
-            <View>
-                <Text>Ini Kata Percobaan: </Text>
-                {this.renderNama()}
-            </View>
+             // <ListView dataSource={this.dataSource} renderRow={this.renderRow}/>
+             <FlatList 
+                data = {this.props.libraries}
+                renderItem = {this.renderRow}
+             />
         );
     }
 }
 
 const mapStateToProps = (state) => {
-    return { kataKu: state.nama }
+    return { libraries: state.libraries }
 }
 
 export default connect(mapStateToProps)(LibraryList);
